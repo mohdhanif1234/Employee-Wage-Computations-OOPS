1b1f22e0591377c5add1244ac5935a82dea1f563
mohdhanif1234 <hanif.topia@gmail.com>
2021-09-26 16:05:42 +0530

    [Hanif]:Added a program to calculate monthly employee wage

---
+++ EmployeeWageOOPS/EmployeeWageOOPS/MonthlyEmpWage.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageOOPS
+{
+    class MonthlyEmpWage
+    {
+
+        // UC-5: Calculating monthly employee wage assuming 20 working days in a month 
+
+        // Constants
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR= 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
+
+        public static void MonthlyEmpWageCalculation()
+        {
+            // Variables
+            int empHrs = 0;
+            int empWage = 0;
+            int totalEmpWage = 0;
+            // Computation
+            Random random = new Random();
+            for (int day = 1; day <= NUM_OF_WORKING_DAYS; day++)
+            {
+                int empCheck = random.Next(0, 3);
+
+                // Getting employee work hours
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("Emp Wage for day " + day + " : Rs " + empWage);
+            }
+
+            // Calculating total employee wage for a month
+            Console.WriteLine("Total Emp Wage : Rs " + totalEmpWage);
+            Console.ReadLine();
+        }
+        
+        static void Main(string[] args)
+        {
+            MonthlyEmpWageCalculation(); // Method calling to calculate monthly employee wage
+        }
+    }
+}
