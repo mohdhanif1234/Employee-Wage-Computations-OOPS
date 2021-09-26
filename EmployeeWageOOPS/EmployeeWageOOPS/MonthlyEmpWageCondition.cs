cc4e5e1e4f09738d4e5eb056bdb21b248e37f92d
mohdhanif1234 <hanif.topia@gmail.com>
2021-09-26 16:37:51 +0530

    [Hanif]:Added a program to calculate monthly wage till a particular condition of hours of day is met

--- EmployeeWageOOPS/EmployeeWageOOPS/MonthlyEmpWageCondition.cs
+++ EmployeeWageOOPS/EmployeeWageOOPS/MonthlyEmpWageCondition.cs
@@ -6,30 +6,35 @@ using System.Threading.Tasks;
 
 namespace EmployeeWageOOPS
 {
-    class MonthlyEmpWage
+    class MonthlyEmpWageCondition
     {
 
-        // UC-5: Calculating monthly employee wage assuming 20 working days in a month 
+        /* UC-6: Calculating monthly employee wage till a condition of either the max working hours or 
+          max number of working days is reached, assuming 20 working days in a month */ 
 
         // Constants
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR= 20;
         public const int NUM_OF_WORKING_DAYS = 20;
+        public const int MAX_HRS_IN_MONTH = 50;
 
-        public static void MonthlyEmpWageCalculation()
+        public static void MonthlyEmpWageCond()
         {
             // Variables
             int empHrs = 0;
-            int empWage = 0;
             int totalEmpWage = 0;
+            int totalWorkingDays = 0;
+            int totalEmpHrs = 0;
+
             // Computation
             Random random = new Random();
-            for (int day = 1; day <= NUM_OF_WORKING_DAYS; day++)
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
             {
+                totalWorkingDays++;
                 int empCheck = random.Next(0, 3);
-
-                // Getting employee work hours
+                
+                // Getting employee hours 
                 switch (empCheck)
                 {
                     case IS_PART_TIME:
@@ -42,19 +47,17 @@ namespace EmployeeWageOOPS
                         empHrs = 0;
                         break;
                 }
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                totalEmpWage += empWage;
-                Console.WriteLine("Emp Wage for day " + day + " : Rs " + empWage);
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day {0}: Emp Hrs : {1}",totalWorkingDays,empHrs);
             }
-
-            // Calculating total employee wage for a month
+            totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Total Emp Wage : Rs " + totalEmpWage);
             Console.ReadLine();
         }
         
         static void Main(string[] args)
         {
-            MonthlyEmpWageCalculation(); // Method calling to calculate monthly employee wage
+            MonthlyEmpWageCond(); // Method calling to calculate monthly employee wage till a condition is meet
         }
     }
 }
