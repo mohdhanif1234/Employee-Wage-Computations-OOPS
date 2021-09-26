f4e42985094b3e46a93710a1eebcb3bb5f83f9eb
mohdhanif1234 <hanif.topia@gmail.com>
2021-09-26 19:55:27 +0530

    [Hanif]:Added a program to calculate employee wage using class methods

--- EmployeeWageOOPS/EmployeeWageOOPS/MonthlyEmpWageCondition.cs
+++ EmployeeWageOOPS/EmployeeWageOOPS/MonthlyEmpWageCondition.cs
@@ -8,56 +8,55 @@ namespace EmployeeWageOOPS
 {
     class MonthlyEmpWageCondition
     {
-
-        /* UC-6: Calculating monthly employee wage till a condition of either the max working hours or 
-          max number of working days is reached, assuming 20 working days in a month */ 
+        //UC-7: Refactor the code to include the class methods  
 
         // Constants
-        public const int IS_PART_TIME = 1;
-        public const int IS_FULL_TIME = 2;
-        public const int EMP_RATE_PER_HOUR= 20;
-        public const int NUM_OF_WORKING_DAYS = 20;
-        public const int MAX_HRS_IN_MONTH = 50;
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int MAX_WORKING_DAYS = 20;
+        public const int MAX_WORKING_HOURS = 100;
 
-        public static void MonthlyEmpWageCond()
+        public static int ClassMethodToComputeEmpWage()
         {
-            // Variables
-            int empHrs = 0;
-            int totalEmpWage = 0;
-            int totalWorkingDays = 0;
-            int totalEmpHrs = 0;
+            // Declaring and initializing local variables
+            int empHr;
+            int day = 1;
+            int totalWorkHr = 0;
 
-            // Computation
             Random random = new Random();
-            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
-            {
-                totalWorkingDays++;
+            // Computation
+            while (day <= MAX_WORKING_DAYS && totalWorkHr <= MAX_WORKING_HOURS)
+            { 
                 int empCheck = random.Next(0, 3);
-                
-                // Getting employee hours 
+
+                // Getting employee work hours
                 switch (empCheck)
                 {
-                    case IS_PART_TIME:
-                        empHrs = 4;
-                        break;
                     case IS_FULL_TIME:
-                        empHrs = 8;
+                        empHr = 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHr = 4;
                         break;
                     default:
-                        empHrs = 0;
+                        empHr = 0;
                         break;
                 }
-                totalEmpHrs += empHrs;
-                Console.WriteLine("Day {0}: Emp Hrs : {1}",totalWorkingDays,empHrs);
+                day++;
+                int dailyWage = EMP_RATE_PER_HOUR * empHr;
+                Console.WriteLine("Daily wage is " + dailyWage);
+                totalWorkHr += empHr;
             }
-            totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Total Emp Wage : Rs " + totalEmpWage);
-            Console.ReadLine();
+            return totalWorkHr;
         }
-        
+
         static void Main(string[] args)
         {
-            MonthlyEmpWageCond(); // Method calling to calculate monthly employee wage till a condition is meet
+            int totalWorkHr = ClassMethodToComputeEmpWage(); // Method calling to get the total employee wage
+            int totalWage = EMP_RATE_PER_HOUR * totalWorkHr;
+            Console.WriteLine("Total wage is : Rs " + totalWage);
+            Console.ReadLine();
         }
     }
 }
