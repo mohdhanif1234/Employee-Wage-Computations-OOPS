af45301230f246332122bbd5f97b9e5d96d23580
mohdhanif1234 <hanif.topia@gmail.com>
2021-09-26 15:45:35 +0530

    [Hanif]:Added a program to calculate part time employee wage

--- EmployeeWageOOPS/EmployeeWageOOPS/PartTimeEmpWage.cs
+++ EmployeeWageOOPS/EmployeeWageOOPS/PartTimeEmpWage.cs
@@ -6,27 +6,34 @@ using System.Threading.Tasks;
 
 namespace EmployeeWageOOPS
 {
-    class DailyEmpWage
+    class PartTimeEmpWage
     {
-        // Constants
-        public const int IS_FULL_TIME = 1;
-        public const int EMP_RATE_PER_HOUR = 20;
 
-        public static void DailyEmpWageCalculation()
+        // UC-3: Calculating part time employee wage
+
+        // Constants
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR= 20;
+
+        public static void PartTimeEmpWageCalculation()
         {
             // Variables
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
-
             // Computation
-            int empCheck = random.Next(0, 2);
+            int empCheck = random.Next(0, 3);
 
             // Getting employee work hours
-            if (empCheck == IS_FULL_TIME)
+            if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
             {
                 empHrs = 8;
-            } 
+            }
             else
             {
                 empHrs = 0;
@@ -38,7 +45,7 @@ namespace EmployeeWageOOPS
         
         static void Main(string[] args)
         {
-            DailyEmpWageCalculation(); // Method calling to calculate daily employee wage
+            PartTimeEmpWageCalculation(); // Method calling to calculate part time employee wage
         }
     }
 }
